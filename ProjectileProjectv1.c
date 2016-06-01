50cd51ad6ccd914c884f8bc00a2d71e7e375006f
Michael Leonffu <leonffu.michael@gmail.com>
2016-06-01 01:18:56 -0700

    Created rough draft for code to test a posible solution with pot

---
+++ ProjectileProjectv1.c
@@ -0,0 +1,66 @@
+
+
+//varlibes:
+
+int MOTORMAXPOWER = 127;
+
+int MOTORCALIPOWER = 25;
+
+int BESTANGLEPOT = 500;
+
+int BESTANGLETHRESHPOT = 100;
+
+//Functions:
+
+void simpleAngleControl{
+	if(SensorValue(buttonUpAngle) == 1){
+		startMotor(motorAngle,-MOTORMAXPOWER);
+	}
+	if(SensorValue(buttonDownAngle) == 1){
+		startMotor(motorAngle,MOTORMAXPOWER);
+	}
+	if(SensorValue(buttonUpAngle) == 0 && SensorValue(buttonDownAngle) == 0){
+		startMotor(motorAngle,0);
+	}
+}
+
+void simpleComplexAngleControl{
+	if(SensorValue(potAngle) < BESTANGLEPOT - BESTANGLETHRESHPOT){
+		startMotor(motorAngle,MOTORCALIPOWER);
+	}
+	if(SensorValue(potAngle) > BESTANGLEPOT + BESTANGLETHRESHPOT){
+		startMotor(motorAngle,-MOTORCALIPOWER);
+	}
+	if(SensorValue(potAngle) > BESTANGLEPOT + BESTANGLETHRESHPOT/2 || SensorValue(potAngle) < BESTANGLEPOT - BESTANGLETHRESHPOT/2){
+		startMotor(motorAngle,0);
+	}
+}
+
+void simpleComplexAngleControlCali{
+	if(SensorValue(buttonUpAngle) == 1){
+		startMotor(motorAngle,-MOTORCALIPOWER);
+	}
+	if(SensorValue(buttonDownAngle) == 1){
+		startMotor(motorAngle,MOTORCALIPOWER);
+	}
+	if(SensorValue(buttonUpAngle) == 0 && SensorValue(buttonDownAngle) == 0){
+		startMotor(motorAngle,0);
+	}
+}
+
+void launcherKick{
+	if(SensorValue(limitLaunch) == 1){
+		startMotor(motorLaunch,MOTORMAXPOWER);
+	}
+	if(SensorValue(limitLaunch) == 0){
+		startMotor(motorLaunch,0);
+	}
+}
+
+
+task main(){
+	simpleAngleControl();
+	//simpleComplexAngleControl();
+	//simpleComplexAngleControlCali();
+	launcherKick();
+}
